ef05f3209e1ae43b6948a5f09175a59b934b2579
mohamedhesham569 <mohmedhesham569@yahoo.com>
2023-12-12 03:09:17 +0200

    new changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
     <Cartprovider>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Home/>} />
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ReactDOM } from 'react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+const root =createRoot(container);
 
-// ReactDOM.render((
+// ReactDOM.render(
 //     <BrowserRouter basename={process.env.PUBLIC_URL}>
 //         <App/>
 //     </BrowserRouter>
-// ),)
-root.render(<App />);
+// ,container)
+root.render(
+    <App/>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
