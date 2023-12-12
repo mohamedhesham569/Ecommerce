4cd32ced768a869dc7b8f44d566d87b464250db0
mohamedhesham569 <mohmedhesham569@yahoo.com>
2023-12-12 02:37:15 +0200

    new changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+// ReactDOM.render((
+//     <BrowserRouter basename={process.env.PUBLIC_URL}>
+//         <App/>
+//     </BrowserRouter>
+// ),)
 root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
